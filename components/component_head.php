d6648a23150662847bdf229efcc904e98484d4aa
dantilse <dantilse@gmail.com>
2016-10-20 10:43:03 -0500

    Updates: - Switched from <legend> to h* with class="x_title" (hard to style legend across browsers) - Fixed issues found in w3c validator, still lots of duplicate IDs in session-create-new (this is just for example) - Updated some grid areas in Sponsor Application (NXP Lists area stays full-width)

--- components/component_head.php
+++ components/component_head.php
@@ -1,4 +1,4 @@
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!--meta http-equiv="Content-Type" content="text/html; charset=UTF-8"-->
 <!-- Meta, title, CSS, favicons, etc. -->
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- components/component_top_nav.php
+++ components/component_top_nav.php
@@ -1,5 +1,5 @@
 <div class="nav_menu">
-  <nav class="" role="navigation">
+  <nav class="">
     <div class="nav toggle">
       <a id="menu_toggle"><i class="fa fa-bars"></i></a>
     </div>
